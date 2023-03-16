fb3316eeaace0729afb671ff221cade37f801c74
tyler ferre <ferre.tyler1@gmail.com>
2023-03-16 15:44:14 -0600

    finished 8 buttons

--- professional-dj/src/App.jsx
+++ professional-dj/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent, useState } from 'react'
+import React, {useState} from 'react'
 import Box from './Box'
 
 
@@ -15,16 +15,46 @@ function App() {
   }
 
   const partyDj = () =>{
-    setColors(prevState => prevState[0] === "purple" ? [...prevState] : ["purple", "purple", prevState[2], prevState[3]])
+    setColors(prevState => prevState[0] === "purple" ? ["white", "white", prevState[2], prevState[3]] : ["purple", "purple", prevState[2], prevState[3]])
   }
 
+  const leftBlue = () =>{
+    setColors(prevState => prevState[2] === "blue" ? [prevState[0], prevState[1], "white", prevState[3]] : [prevState[0], prevState[1], "blue", prevState[3]])
+  }
 
+  const rightBlue = () =>{
+    setColors(prevState => prevState[3] === "blue" ? [prevState[0], prevState[1], prevState[2], "white"] : [prevState[0], prevState[1], prevState[2], "blue"])
+
+  }
+
+  const leftYellow = () =>{
+    setColors(prevState => prevState[0] === "yellow" ? ["white", prevState[1], prevState[2], prevState[3]] : ["yellow", prevState[1], prevState[2], prevState[3]])
+  }
+
+  const rightOrange = () =>{
+    setColors(prevState => prevState[1] === "orange" ? [prevState[0], "white", prevState[2], prevState[3]] : [prevState[0], "orange", prevState[2], prevState[3]])
+
+  }
+
+  const leftPink = () =>{
+    setColors(prevState => prevState[2] === "pink" ? [prevState[0], prevState[1], "white", prevState[3]] : [prevState[0], prevState[1], "pink", prevState[3]])
+  }
+
+  const rightGreen = () =>{
+    setColors(prevState => prevState[3] === "green" ? [prevState[0], prevState[1], prevState[2], "white"] : [prevState[0], prevState[1], prevState[2], "green"])
+  }
 
   return (
     <div className="App">
       {boxElements}
       <button className='smallDj' onClick={smallDj}>Small DJ</button>
       <button className='partyDj' onClick={partyDj}>Party DJ</button>
+      <button className='leftBlue' onClick={leftBlue}>Left Blue</button>
+      <button className='rightBlue' onClick={rightBlue}>Right Blue</button>
+      <button className='leftYellow' onClick={leftYellow}>Left Yellow</button>
+      <button className="rightOrange" onClick={rightOrange}>Right Orange</button>
+      <button className='leftPink' onClick={leftPink}>Left Pink</button>
+      <button className='rightGreen' onClick={rightGreen}>Right Green</button>
     </div>
   )
 }
