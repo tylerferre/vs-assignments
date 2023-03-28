d7a6a5070382db6ffeac170f8aa1cea6e4f318d4
tyler ferre <ferre.tyler1@gmail.com>
2023-03-28 14:29:44 -0600

    fixed code so it only makes one get request

--- random-color-component/src/App.jsx
+++ random-color-component/src/App.jsx
@@ -1,31 +1,65 @@
+// import React, { useState, useEffect } from 'react'
+// import axios from 'axios'
+
+// function App() {
+
+// const [color, setColor] = useState('')
+// const [randomNum, setRandomNum] = useState(0)
+
+//   const randomize = () =>{
+//   setRandomNum(Math.floor(Math.random() * 7) + 1)
+// }
+
+// useEffect(() =>{
+//   axios.get('https://www.colr.org/json/schemes/random/7?scheme_size_limit=>5')
+//   .then(res => {
+//     setColor(res.data.matching_schemes[randomNum])
+//     //console.log(res.data)
+//   })
+//   .catch(err => console.log(err))
+// }, [randomNum])
+
+
+//   return (
+//     <div className="App">
+//       <div className='color' style={{backgroundColor: `#${color}`}}></div>
+//       <button onClick={randomize} style={{borderColor: `#${color}`}}>Click</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import axios, { all } from 'axios'
 
 function App() {
 
-const [color, setColor] = useState('')
-const [randomNum, setRandomNum] = useState(0)
-
-  const randomize = () =>{
-  setRandomNum(Math.floor(Math.random() * 7) + 1)
-}
+const [allColors, setAllColors] = useState([])
+const [color, setColor] = useState([])
 
 useEffect(() =>{
   axios.get('https://www.colr.org/json/schemes/random/7?scheme_size_limit=>5')
-  .then(res => {
-    setColor(res.data.matching_schemes[randomNum])
-    //console.log(res.data)
-  })
-  .catch(err => console.log(err))
-}, [randomNum])
+    .then(res => setAllColors(res.data.matching_schemes))
+    .catch(err => console.log(err))
+}, [])
 
+const getColor = () =>{
+  const randomNum = Math.floor(Math.random() * allColors.length)
+  const newColor = allColors[randomNum]
+  console.log(color)
+  setColor([newColor])
+}
 
   return (
     <div className="App">
       <div className='color' style={{backgroundColor: `#${color}`}}></div>
-      <button onClick={randomize} style={{borderColor: `#${color}`}}>Click</button>
+      <button onClick={getColor} style={{borderColor: `#${color}`}}>Click</button>
     </div>
   )
 }
 
 export default App
+
